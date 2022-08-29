a0fbd62338e5627918c90c788ca67e1a8543e505
Kota <maedakoutaa@gmail.com>
2022-08-29 10:52:45 +0900

    Initial Commit

---
+++ MiniApp89-SwiftUIWithStoryboard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MiniApp89-SwiftUIWithStoryboard
+//
+//  Created by 前田航汰 on 2022/08/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MiniApp89-SwiftUIWithStoryboard/MiniApp89_SwiftUIWithStoryboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp89_SwiftUIWithStoryboardApp.swift
+//  MiniApp89-SwiftUIWithStoryboard
+//
+//  Created by 前田航汰 on 2022/08/29.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp89_SwiftUIWithStoryboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
